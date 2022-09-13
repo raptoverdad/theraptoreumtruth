af2b1ae924f2e28d4ae28e50e34317fbc4ae5836
RMwebservices <rodrigomreidenbach@gmail.com>
2022-09-13 03:47:52 -0300

    ++++ steroids

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(store).use(router).mount('#app')
+Vue.use(VueAxios, axios)
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,89 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="wrapper">
+     <div class="container">One</div>
+     <div class="container">Two</div>
+     <div class="container">Three</div>
+     <div class="container">Four</div>
+     <div class="container">Five</div>
+     <div class="container">Six</div>
+    </div>
+  </div>
+
+  <div class="footer">
+    <div class="time">powered by Rapto Verdad</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
-  name: 'HomeView',
+  name: 'homeView',
   components: {
-    HelloWorld
+
+  },
+  methods:{
+
   }
 }
 </script>
+<style>
+
+
+
+  .home{
+    height: min-content;
+    width: 100vw;
+
+  
+  }
+.wrapper{
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 10px; /*The space between grid containers*/
+  grid-auto-rows:minmax(50vh,50vh);
+  animation: background 3s infinite;
+  padding: 0.5% 0;
+  max-width: 100vw;
+ 
+}
+.container{
+  border: 1px solid #000;
+background-color: #111;
+cursor: pointer;
+}
+.footer{
+  position:sticky;
+  bottom: 0;
+  width:100vw ;
+  height: 5vh;
+  background-color: #000;
+  background: rgba(0, 0,0, 1);
+}
+  .time{
+    display: flex;
+    justify-content:center ;
+    align-items: center;
+    max-width:100%;
+    height: 100%;
+    color: #fff;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    cursor: pointer;
+  }
+  @keyframes background{
+0%{
+background-color: #000;
+}
+50%{
+  background-color: #f00;
+}
+100%{
+  background-color: #000;
+}
+}
+</style>
